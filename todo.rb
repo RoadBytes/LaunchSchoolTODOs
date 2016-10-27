c984b57a7d35d28aa8c6ab5d7dd2d43451aa87f9
Jason Data <jason.data@roadbytes.me>
2016-10-27 18:58:02 +1100

    CRUD Lists

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/content_for'
 require 'tilt/erubis'
 
 set :server, 'webrick'
@@ -32,6 +33,16 @@ def error_for_list_name(name)
   end
 end
 
+def error_for_edit_list_name(old_name, new_name)
+  # take out list with name first
+  other_lists = session[:lists].select { |list| list[:name] != old_name }
+  if !(1..100).cover? new_name.size
+    'Name must be 1 to 100 chars long'
+  elsif other_lists.any? { |list| list[:name] == new_name }
+    'Name already taken'
+  end
+end
+
 # New
 get '/lists/new' do
   erb :lists_new
@@ -54,10 +65,58 @@ post '/lists' do
   end
 end
 
-# Delete
-post '/lists/:name' do
-  name = params[:name]
-  session[:lists].reject! { |list| list[:name] == name }
+# List Delete
+post '/lists/:id/delete' do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = 'List Deleted'
 
   redirect '/lists'
+end
+
+# List Show
+get '/lists/:id' do
+  @id = params[:id].to_i
+  @list = session[:lists][@id]
+
+  erb :list
+end
+
+# List Edit
+get '/lists/:id/edit' do
+  @id   = params[:id].to_i
+  @list = session[:lists][@id]
+
+  erb :list_edit
+end
+
+# List Update
+post '/lists/:id' do
+  @id   = params[:id].to_i
+  @list = session[:lists][@id]
+
+  @old_name = @list[:name]
+  @new_name = params[:list_name].strip
+
+  error = error_for_edit_list_name(@old_name, @new_name)
+  if error
+    session[:error] = error
+
+    erb :list_edit
+  else
+    @list[:name] = @new_name
+    session[:success] = 'List Updated'
+
+    redirect "/lists/#{@id}"
+  end
+end
+
+# Todo Create
+post '/lists/:id/todo' do
+  @name = params[:name]
+  @todo = params[:todo]
+  @list = session[:lists].find { |list| list[:name] == @name }
+  @list[:todos] << @todo
+
+  erb :list
 end
